fc6d1cb431d17f800bb1608e2eb5f26afbf7d09e
niu <chillmann@foxmail.com>
2020-07-05 15:34:54 +0800

    56

---
+++ src/main/java/niu/study/swordpointoffer/Solution56_I.java
@@ -0,0 +1,33 @@
+package niu.study.swordpointoffer;
+
+public class Solution56_I {
+    public int[] singleNumbers(int[] nums) {
+        //用于将所有的数异或起来
+        int k = 0;
+
+        for(int num: nums) {
+            k ^= num;
+        }
+
+        //获得k中最低位的1
+        int mask = 1;
+
+        //mask = k & (-k) 这种方法也可以得到mask，具体原因百度 哈哈哈哈哈
+        while((k & mask) == 0) {
+            mask <<= 1;
+        }
+
+        int a = 0;
+        int b = 0;
+
+        for(int num: nums) {
+            if((num & mask) == 0) { //分组
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
