81520e1baba9971b8d5e7c9420797cbc251d9bb1
niu <chillmann@foxmail.com>
2020-07-07 10:46:50 +0800

    58

---
+++ src/main/java/niu/study/swordpointoffer/Solution58_II.java
@@ -0,0 +1,10 @@
+package niu.study.swordpointoffer;
+
+public class Solution58_II {
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder res = new StringBuilder();
+        for(int i = n; i < n + s.length(); i++)
+            res.append(s.charAt(i % s.length()));
+        return res.toString();
+    }
+}
