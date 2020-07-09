9aae968eae14b5adae56b9e08f8e02b023dba841
niu <chillmann@foxmail.com>
2020-07-09 14:04:19 +0800

    剑指刷完啦 撒花~

--- src/main/java/niu/study/swordpointoffer/Solution68_II.java
+++ src/main/java/niu/study/swordpointoffer/Solution68_II.java
@@ -4,12 +4,21 @@ import niu.study.Leetcode.TreeNode;
 
 public class Solution68_II {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) return root;
+        if (root == null || root == p || root == q) return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left == null) return right;
-        if(right == null) return left;
+        if (left == null) return right;
+        if (right == null) return left;
         return root;
+    }
+    public TreeNode annotation(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = annotation(root.left, p, q);
+        TreeNode right = annotation(root.right, p, q);
+        if(left == null && right == null) return null; // 1.
+        if(left == null) return right; // 3.
+        if(right == null) return left; // 4.
+        return root; // 2. if(left != null and right != null)
     }
 
 }
